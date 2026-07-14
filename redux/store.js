95444dbdcd2fffb6d3e03135d796062ae77a77ce
Shivam Sinha <sinhashivam04@gmail.com>
2026-07-15 02:23:15 +0530

    redux logger

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import User from "./reducers/User";
+import { logger } from 'redux-logger';
 
 const rootReducer = combineReducers({
     user: User
 })
 
 const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: getDefaultMiddleWare => {
+        return getDefaultMiddleWare().concat(logger)
+    }
 })
 
 export default store;
