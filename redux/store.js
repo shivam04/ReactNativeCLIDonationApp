8bc9d9e52a443da4a74ade8b7f2f16767d08c71b
Shivam Sinha <sinhashivam04@gmail.com>
2026-07-15 02:38:03 +0530

    redux async storage

--- redux/store.js
+++ redux/store.js
@@ -1,16 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import User from "./reducers/User";
-import { logger } from 'redux-logger';
 
 const rootReducer = combineReducers({
     user: User
 })
 
 const store = configureStore({
-    reducer: rootReducer,
-    // middleware: getDefaultMiddleWare => {
-    //     return getDefaultMiddleWare().concat(logger)
-    // }
+    reducer: rootReducer
 })
 
 export default store;
